cd242a0965b7e2299da4fef12e6c37df865c6685
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-09-04 22:23:35 +0100

    style the "Browse Textbooks" menu modal and add js restrictions on search submit

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,22 +1,43 @@
-const browseLink = document.querySelector(".browse-link");
-console.log(browseLink)
-const browse = document.querySelector(".browse");
+const browseLink = document.querySelectorAll(".browse-link");
+const browse = document.getElementById("browse");
 
-export function add(a, b) {
-	return a + b;
-}
-
+/** Opens the search browser menu */
 export function openBrowse() {
-    const overlay = document.createElement('div');
-    overlay.classList.add('overlay');
+    const overlayEl = document.createElement('div');
+    document.body.appendChild(overlayEl);
+    overlayEl.classList.add('overlay');
     browse.classList.add("browse-open");
-    overlay.addEventListener('click', () => {
+    overlayEl.addEventListener('click', () => {
         browse.classList.remove("browse-open")
-        overlay.remove();
+        overlayEl.remove();
     })
 }
 
+browseLink.forEach(link => link.addEventListener('click', openBrowse));
 
 
-browseLink.addEventListener('click', openBrowse)
+const inputElement = document.getElementById('search-input');
+const searchForm = document.getElementById('search-form');
+const searchButton = document.getElementById('search-button');
+
+/** Submit the search form only if there is a value in the field and the
+ * Enter key / button icon was pressed, otherwise
+ * disable the search icon button.
+ */
+inputElement.addEventListener('change', event => {
+    if (event.target.value.length === 0) {
+			searchButton.disabled = true;
+		} else {
+			searchButton.disabled = false;
+		}
+	if (event.key === 'Enter') {
+		event.preventDefault();
+
+		if (event.target.value.length === 0) {
+			return;
+		} else {
+			searchForm.submit();
+		}
+	}
+});
 
