30cf61df5262172b1ef4da28f77a947464d5a267
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-09-05 15:49:21 +0100

    customise book and stock admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
 from .models import YearGroup, Subject, Book, Stock
 
-# Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'year_group',
+        'subject',
+        'exam_board',
+        'publisher',
+        'image'
+    )
+
+    ordering = ('year_group', 'subject')
+
+
+class StockAdmin(admin.ModelAdmin):
+    list_display = (
+        'book',
+        'condition',
+        'price',
+        'quantity'
+    )
+
+
 admin.site.register(YearGroup)
 admin.site.register(Subject)
-admin.site.register(Book)
-admin.site.register(Stock)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Stock, StockAdmin)
