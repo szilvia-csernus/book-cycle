150f2210db65570208de2fe805962648e02deb4d
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-09-09 23:18:58 +0100

    improve plus-minus buttons functionality

--- static/js/book_details.js
+++ static/js/book_details.js
@@ -1,6 +1,6 @@
-const buttonNew = document.getElementById("button-new");
-const buttonGood = document.getElementById("button-good");
-const buttonFair = document.getElementById("button-fair");
+const buttonNew = document.getElementById("add-button-new");
+const buttonGood = document.getElementById("add-button-good");
+const buttonFair = document.getElementById("add-button-fair");
 
 const stock = {
 	new: buttonNew?.dataset.stockQuantity,
@@ -11,18 +11,67 @@ const stock = {
 function addMoreBookToBasket(id, key) {
     const counter = document.getElementById(`counter-${id}`);
     const stockElement = document.getElementById(`stock-${id}`);
-    counter.textContent = counter.textContent ++;
+    const plusButton = document.getElementById(`plus-${id}`);
+    counter.textContent = Number(counter.textContent) + 1;
     stock[key]--;
     if (stock[key] > 0) {
-        counter.textContent = Number(counter.textContent) + 1;
-        stockElement.textContent = `${stock[key]} more in stock`;
+        if (Number(counter.textContent) > 10) {
+            stockElement.textContent = '10+ in stock';
+        } else {
+            stockElement.textContent = `${stock[key]} more in stock`;
+        }
+        plusButton.disabled = false;
     }
     else {
         stockElement.textContent = `no more left in stock`;
-        counter.textContent = Number(counter.textContent) + 1;
-        const plusBtn = document.getElementById(`plus-${id}`)
-        plusBtn.disabled = true;
+        
+        plusButton.disabled = true;
     }
+}
+
+// function recreteAddButton(id, key) {
+//     const button = document.createElement('button');
+//     button.id = `button-${key}`;
+//     button.dataset.stockId = id;
+//     button.dataset.stockQuantity = stock[key];
+//     button.classList.add('stockline-button');
+//     button.textContent = 'Add';
+    
+//     stockElement.textContent = `\u2713 in stock`;
+//     button.addEventListener('click', () => {
+//         const replaceElement = replaceButtons(id, key);
+//         button.replaceWith(replaceElement);
+//         const stockElement = document.getElementById(`stock-${id}`);
+//         stockElement.textContent = `${stock[key]} more in stock`;
+//     });
+
+//     return button
+// }
+
+function removeBookFromBasket(id, key) {
+	const counter = document.getElementById(`counter-${id}`);
+	const stockElement = document.getElementById(`stock-${id}`);
+    const minusButton = document.getElementById(`minus-${id}`);
+    const plusButton = document.getElementById(`plus-${id}`);
+	counter.textContent = Number(counter.textContent) - 1;
+    console.log(counter.textContent)
+	stock[key]++;
+	if (counter.textContent === "0") {
+        const replaceElement = key === "new" ? buttonNew :
+            key === "good" ? buttonGood : buttonFair
+        replaceElement.style.display = "block";
+        stockElement.textContent = `\u2713 in stock`;
+        minusButton.remove();
+        counter.remove();
+        plusButton.remove();
+		
+	} else {
+        if (Number(counter.textContent) > 10) {
+                stockElement.textContent = '10+ in stock';
+            } else {
+                stockElement.textContent = `${stock[key]} more in stock`;
+            }
+	}
 }
 
 function createPlusButton(id, key) {
@@ -34,18 +83,20 @@ function createPlusButton(id, key) {
     return plusButton
 }
 
-function createMinusButton(id) {
+function createMinusButton(id, key) {
     const minusButton = document.createElement('button')
     minusButton.classList.add("stockline-button");
     minusButton.textContent = "-"
     minusButton.id = `minus-${id}`;
+    minusButton.addEventListener('click', () => removeBookFromBasket(id, key))
     return minusButton
 }
 
-function createCounter(id) {
+function createCounter(id, key) {
     const counter = document.createElement('strong');
     counter.textContent = "1";
     counter.id = `counter-${id}`;
+    stock[key]--;
     return counter
 }
 
@@ -54,7 +105,7 @@ function replaceButtons(id, key) {
     replaceElement.classList.add("replace-element")
     const minus = createMinusButton(id, key);
     replaceElement.append(minus);
-    const counter = createCounter(id);
+    const counter = createCounter(id, key);
     replaceElement.append(counter);
     const plus = createPlusButton(id, key);
     replaceElement.append(plus);
@@ -65,23 +116,66 @@ function replaceButtons(id, key) {
 buttonNew?.addEventListener('click', event => {
     const id = buttonNew.dataset.stockId;
     const replaceElement = replaceButtons(id, "new")
-    buttonNew.replaceWith(replaceElement)
+    buttonNew.parentNode.append(replaceElement);
+    buttonNew.style.display = "none"
     const stockElement = document.getElementById(`stock-${id}`);
     stockElement.textContent = `${stock.new} more in stock`
+    const plusButton = document.getElementById(`plus-${id}`);
+    if (stock.new > 0) {
+			if (Number(counter.textContent) > 10) {
+				stockElement.textContent = '10+ in stock';
+			} else {
+				stockElement.textContent = `${stock.new} more in stock`;
+			}
+			plusButton.disabled = false;
+		} else {
+			stockElement.textContent = `no more left in stock`;
+
+			plusButton.disabled = true;
+		}
+
 })
 
 buttonGood?.addEventListener('click', event => {
     const id = buttonGood.dataset.stockId;
     const replaceElement = replaceButtons(id, "good")
-    buttonGood.replaceWith(replaceElement)
+    buttonGood.parentNode.append(replaceElement);
+    buttonGood.style.display = "none";
     const stockElement = document.getElementById(`stock-${id}`);
     stockElement.textContent = `${stock.good} more in stock`
+    const plusButton = document.getElementById(`plus-${id}`);
+    if (stock.good > 0) {
+			if (Number(counter.textContent) > 10) {
+				stockElement.textContent = '10+ in stock';
+			} else {
+				stockElement.textContent = `${stock.good} more in stock`;
+			}
+			plusButton.disabled = false;
+		} else {
+			stockElement.textContent = `no more left in stock`;
+
+			plusButton.disabled = true;
+		}
 })
 
 buttonFair?.addEventListener('click', event => {
     const id = buttonFair.dataset.stockId;
     const replaceElement = replaceButtons(id, "fair")
-    buttonFair.replaceWith(replaceElement)
+    buttonFair.parentNode.append(replaceElement)
+    buttonFair.style.display = "none";
     const stockElement = document.getElementById(`stock-${id}`);
-    stockElement.textContent = `${stock.fair} more in stock`
+    stockElement.textContent = `${stock.fair} more in stock`;
+    const plusButton = document.getElementById(`plus-${id}`);
+    if (stock.fair > 0) {
+			if (Number(counter.textContent) > 10) {
+				stockElement.textContent = '10+ in stock';
+			} else {
+				stockElement.textContent = `${stock.fair} more in stock`;
+			}
+			plusButton.disabled = false;
+		} else {
+			stockElement.textContent = `no more left in stock`;
+
+			plusButton.disabled = true;
+		}
 })
