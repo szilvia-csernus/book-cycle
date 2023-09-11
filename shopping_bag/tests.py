fdcfa975f5bdb3f1008b640b6396d02f3631cdec
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-09-11 22:18:47 +0100

    add test cases to add_to_bag function

--- shopping_bag/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-
-class ShoppingBagViewTestCase(TestCase):
-
-    def test_shopping_bag(self):
-        response = self.client.get('/shopping_bag/')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'shopping_bag/bag.html')
--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_bag(request):
     """ A view to return the shopping bag page """
 
     return render(request, 'shopping_bag/bag.html')
+
+
+def add_to_bag(request, stock_id):
+    """ Add one book to the shopping bag """
+
+    bag = request.session.get('bag', {})
+    redirect_url = request.POST.get('redirect_url')
+
+    if stock_id in list(bag.keys()):
+        bag[stock_id] += 1
+    else:
+        bag[stock_id] = 1
+
+    request.session['bag'] = bag
+    return redirect(redirect_url)
