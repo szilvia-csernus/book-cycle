d7a71d29f7afccf76fe965efe3a63c372d70fada
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-09-30 12:57:39 +0100

    add image to edit_book page and improve styles

--- inventory/forms.py
+++ inventory/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import YearGroup, Subject, Book, Stock
 
 
@@ -15,6 +16,9 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         exclude = ('image_url', 'product_url', 'slug',)
+
+    image = forms.ImageField(label='Image', required=False,
+                             widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
