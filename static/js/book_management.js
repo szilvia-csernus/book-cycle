bf3bf8656c8b5a27141cf1e81443039715a2b12a
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-09-30 16:01:23 +0100

    custom style image upload button and set behaviour

---
+++ static/js/book_management.js
@@ -0,0 +1,6 @@
+const image = document.getElementById('new-image');
+const filename = document.getElementById('filename');
+image?.addEventListener('change', () => {
+    const file = image.files[0]?.name;
+    filename.textContent = 'Image will be set to: ' + file;
+})
