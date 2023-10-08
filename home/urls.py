3b8336a4309c483816995e4ddb638c72e0aedb2b
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-10-08 22:51:26 +0100

    add privacy notice

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('privacy_notice/', views.privacy_notice, name='privacy_notice')
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def home(request):
     """ A view to return the home page """
     return render(request, 'home/home.html')
+
+
+def privacy_notice(request):
+    """ A view to return the privacy notice page """
+    return render(request, 'home/privacy_notice.html')
