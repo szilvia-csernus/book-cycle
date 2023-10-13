fdfeefaabb98c1eae773e935fabb429e2dc306f2
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-10-13 10:22:32 +0100

    fix bugs related to user uploaded images

--- static/js/book_management.js
+++ static/js/book_management.js
@@ -5,3 +5,24 @@ image?.addEventListener('change', () => {
     const file = image.files[0]?.name;
     filename.textContent = 'Image will be set to: ' + file;
 })
+
+// If user selects a new image, uncheck the "Remove" input field
+
+const clearImageCkeckbox = document.getElementById('image-clear_id');
+const fileName = document.getElementById('filename');
+image?.addEventListener('change', () => {
+	if (image.files.length > 0) {
+        if (clearImageCkeckbox) {
+        clearImageCkeckbox.checked = false;
+        }
+    }
+});
+
+clearImageCkeckbox?.addEventListener('change', () => {
+    if (clearImageCkeckbox.checked === true) {
+        console.log(image.files.length)
+        image.value = '';
+        console.log(image.files);
+        filename.textContent = '';
+    }
+})
