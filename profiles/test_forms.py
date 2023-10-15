2e461418f17fb851fa8d67b92b69957957a039f5
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-10-15 22:45:21 +0100

    add test cases for profile forms

---
+++ profiles/test_forms.py
@@ -0,0 +1,76 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from profiles.forms import UserProfileForm
+
+
+class UserProfileFormTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='emily',
+                                             password='emilyspassword')
+        self.user_profile = self.user.userprofile
+
+    def test_user_profile_form_valid_data(self):
+        form_data = {
+            'default_phone_number': '1234567890',
+            'default_country': 'GB',
+            'default_postcode': '12345',
+            'default_town_or_city': 'City',
+            'default_street_address1': 'asdfg',
+            'default_street_address2': '',
+            'default_county': '',
+        }
+        form = UserProfileForm(data=form_data, instance=self.user_profile)
+        self.assertTrue(form.is_valid())
+
+    def test_user_profile_form_default_country(self):
+        form = UserProfileForm(instance=self.user_profile)
+        self.assertEqual(form.initial['default_country'], 'GB')
+
+    def test_user_profile_form_placeholder_and_classes(self):
+        form = UserProfileForm(instance=self.user_profile)
+        self.assertEqual(
+            form.fields['default_phone_number'].widget.attrs['placeholder'],
+            'Phone Number')
+        self.assertEqual(
+            form.fields['default_phone_number'].widget.attrs['class'],
+            'account-form-input')
+        self.assertEqual(
+            form.fields['default_postcode'].widget.attrs['placeholder'],
+            'Postcode')
+        self.assertEqual(
+            form.fields['default_postcode'].widget.attrs['class'],
+            'account-form-input')
+        self.assertEqual(
+            form.fields['default_town_or_city'].widget.attrs['placeholder'],
+            'Town or City')
+        self.assertEqual(
+            form.fields['default_town_or_city'].widget.attrs['class'],
+            'account-form-input')
+        self.assertEqual(
+            form.fields['default_street_address1'].widget.attrs['placeholder'],
+            '1st Line of Address')
+        self.assertEqual(
+            form.fields['default_street_address1'].widget.attrs['class'],
+            'account-form-input')
+        self.assertEqual(
+            form.fields['default_street_address2'].widget.attrs['placeholder'],
+            '2nd Line of Address')
+        self.assertEqual(
+            form.fields['default_street_address2'].widget.attrs['class'],
+            'account-form-input')
+        self.assertEqual(
+            form.fields['default_county'].widget.attrs['placeholder'],
+            'County')
+
+    def test_user_profile_form_labels(self):
+        form = UserProfileForm(instance=self.user_profile)
+        self.assertEqual(form.fields['default_phone_number'].label,
+                         'Phone Number')
+        self.assertEqual(form.fields['default_postcode'].label, 'Postcode')
+        self.assertEqual(form.fields['default_town_or_city'].label,
+                         'Town or City')
+        self.assertEqual(form.fields['default_street_address1'].label,
+                         '1st Line of Address')
+        self.assertEqual(form.fields['default_street_address2'].label,
+                         '2nd Line of Address')
+        self.assertEqual(form.fields['default_county'].label, 'County')
