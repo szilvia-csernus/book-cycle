bb026c23e33e55aa1c55fe2cde0749523f3fd90c
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-10-19 11:59:23 +0100

    update book_detail url

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('books/', views.all_books, name='books'),
-    path('books/<slug:slug>/', views.book_detail, name='book_detail'),
+    path('book_detail/<slug:slug>/', views.book_detail, name='book_detail'),
     path('add_book/', views.add_book, name='add_book'),
     path('edit_book/<slug:slug>/', views.edit_book, name='edit_book'),
     path('delete_book/<slug:slug>/', views.delete_book, name='delete_book'),
