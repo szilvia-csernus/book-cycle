c37e273c8be715f9ef5a65c65e5d8155144ff276
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2023-10-21 15:54:39 +0100

    remove signal that didn't work with AWS Buckets

--- inventory/signals.py
+++ inventory/signals.py
@@ -1,40 +1,8 @@
-from django.db.models.signals import pre_save, pre_delete
+from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 from django.contrib.sessions.models import Session
-from pathlib import Path
 
-from PIL import Image, ImageOps
-
-from .models import Book, Stock
-
-
-@receiver(pre_save, sender=Book)
-def resize_and_convert_image(sender, instance, **kwargs):
-    """
-    Resize and convert the uploaded image to webp format.
-    """
-    if instance.image:
-        try:
-            # Open the uploaded image using Pillow
-            img = Image.open(instance.image)
-
-            # Apply EXIF orientation (auto-rotate)
-            img = ImageOps.exif_transpose(img)
-
-            # Resize while preserving aspect ratio
-            if instance.image.height > 368 or instance.image.width > 320:
-                img.thumbnail((320, 368))
-
-            # Save the resized image back to the same path
-            source = Path(instance.image.path)
-            destination = source.with_suffix(".webp")
-            img.save(destination, format="webp")
-
-            instance.image = destination.name
-
-        except Exception:
-            # If there is any error, do not save the image
-            pass
+from .models import Stock
 
 
 @receiver(pre_delete, sender=Session)
