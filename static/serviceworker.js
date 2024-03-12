910d5be8709ece1d141428f76da24b2febb2ae73
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-03-12 14:03:02 +0000

    edit serviceworker to include static-file-urls and change caching strategy to cache first

--- static/serviceworker.js
+++ static/serviceworker.js
@@ -1,75 +1,47 @@
 // v1
 
-const assets = [
-	'/',
-	'/static/css/account.css',
-    '/static/css/base.css',
-    '/static/css/checkout.css',
-    '/static/css/home.css',
-    '/static/css/inventory.css',
-    '/static/css/loader.css',
-    '/static/css/modal.css',
-    '/static/css/orders.css',
-    '/static/css/shopping_bag.css',
-    '/static/js/bag.js',
-    '/static/js/book_management.js',
-    '/static/js/books.js',
-    '/static/js/checkout.js',
-    '/static/js/menu.js',
-    '/static/js/modal.js',
-    '/static/js/stripe_elements.js',
-    '/static/js/toast.js',
-	'sw-register.js',
-	'https://fonts.googleapis.com/css2?family=KoHo:wght@300;500;600&family=Koulen&display=swap'
+const static_files = [
+    'https://fonts.googleapis.com/css2?family=KoHo:wght@300;500;600&family=Koulen&display=swap',
 ];
 
+const image_files = [];
+
 // Service Worker Installation
-// 'self' used instead of 'this', it's safer to use
-// self is the service worker itself, it's a different thread from the main thread.
 self.addEventListener('install', (event) => {
+	const fetch_static_files = async () => {
+		const response = await fetch('/static-file-urls/');
+		const data = await response.json();
+		static_files.push(...data);
+		return static_files;
+	};
+
+	const fetch_image_files = async () => {
+		const response = await fetch('/inventory/book-image-urls/');
+		const data = await response.json();
+		image_files.push(...data);
+		return image_files;
+	};
+
 	event.waitUntil(
-		caches.open('assets').then((cache) => {
-			// Fetch the list of static files from your API
-			fetch('/inventory/book-image-urls/')
-				.then((response) => {
-					if (!response.ok) {
-						throw new Error('Network response was not ok');
-					}
-					return response.json();
-				})
-				.then((imageUrls) => {
-					// Add the image urls to the assets array
-					assets.push(...imageUrls);
-					// Cache all assets
-					return cache.addAll(assets);
-				})
-				.catch((error) => {
-					console.error(
-						'There has been a problem with your fetch operation:',
-						error
-					);
-				});
+		caches.open('static_files').then(async (cache) => {
+			const staticFiles = await fetch_static_files();
+			return cache.addAll(staticFiles);
+		})
+	);
+
+	event.waitUntil(
+		caches.open('image_files').then(async (cache) => {
+			const imageFiles = await fetch_image_files();
+			return cache.addAll(imageFiles);
 		})
 	);
 });
 
-
-// Stale while revalidate strategy
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then( cachedResponse => {
-                // Even if the response is in the cache, we fetch it
-                // and update the cache for future usage
-                const fetchPromise = fetch(event.request).then(
-                     networkResponse => {
-                        caches.open("assets").then( cache => {
-                            cache.put(event.request, networkResponse.clone());
-                        });
-                    return networkResponse;
-                    });
-                // We use the currently cached version if it's there
-                return cachedResponse || fetchPromise; // cached or a network fetch
-            })
-        );
-    });
+// Cache first strategy
+self.addEventListener('fetch', (event) => {
+	event.respondWith(
+		caches.match(event.request).then((response) => {
+			return response || fetch(event.request);
+		})
+	);
+});
