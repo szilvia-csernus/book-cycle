ec18c392d8c94c74dbe267ff9d8ab4dd5d369cfa
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-03-13 16:30:39 +0000

    set 'cors' in service worker's fetch calls

--- home/serviceworker.js
+++ home/serviceworker.js
@@ -41,7 +41,7 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
 	event.respondWith(
 		caches.match(event.request).then((response) => {
-			return response || fetch(event.request);
+			return response || fetch(event.request, { mode: 'cors' });
 		})
 	);
 });
