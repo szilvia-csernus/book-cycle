f53f6e2bb7e6ee530a85ce0a168466cc3803c2a0
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-03-13 17:52:17 +0000

    add redirect:follow to service worker fetch requests

--- home/serviceworker.js
+++ home/serviceworker.js
@@ -41,7 +41,9 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
 	event.respondWith(
 		caches.match(event.request).then((response) => {
-			return response || fetch(event.request, { mode: 'cors' });
+			return (
+				response || fetch(event.request, { mode: 'cors', redirect: 'follow' })
+			);
 		})
 	);
 });
