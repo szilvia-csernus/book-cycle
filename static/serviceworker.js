f9b1b5664b006c0af8fbc32e62b7d1c5e883bbd1
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-08-31 18:03:29 +0100

    move serviceworker.js and update settings

---
+++ static/serviceworker.js
@@ -0,0 +1,85 @@
+// v1
+
+const staticFilesUrls = [
+  "https://fonts.googleapis.com/css2?family=KoHo:wght@300;500;600&family=Koulen&display=swap",
+];
+
+const imageFilesUrls = [];
+const cachedUrls = new Set([...staticFilesUrls]);
+
+// Service Worker Installation
+self.addEventListener("install", (event) => {
+  const fetchStaticFileURLs = async () => {
+    const response = await fetch("/static-file-urls/", {
+      mode: "cors",
+      credentials: "omit",
+    });
+    const data = await response.json();
+    staticFilesUrls.push(...data);
+    cachedUrls.add(...data);
+    return staticFilesUrls;
+  };
+
+  const fetchImageFileURLs = async () => {
+    const response = await fetch("/inventory/book-image-urls/", {
+      mode: "cors",
+      credentials: "omit",
+    });
+    const data = await response.json();
+    imageFilesUrls.push(...data);
+    cachedUrls.add(...data);
+    return imageFilesUrls;
+  };
+
+  event.waitUntil(
+    caches.open("static_files").then(async (cache) => {
+      const urls = await fetchStaticFileURLs();
+      const corsRequests = urls.map(
+        (url) => new Request(url, { mode: "cors", credentials: "omit" })
+      );
+      return await Promise.all(
+        corsRequests.map((request) =>
+          fetch(request).then((response) => cache.put(request, response))
+        )
+      );
+    })
+  );
+
+  event.waitUntil(
+    caches.open("image_files").then(async (cache) => {
+      const urls = await fetchImageFileURLs();
+      const corsRequests = urls.map(
+        (url) => new Request(url, { mode: "cors", credentials: "omit" })
+      );
+      return Promise.all(
+        corsRequests.map((request) =>
+          fetch(request).then((response) => cache.put(request, response))
+        )
+      );
+    })
+  );
+});
+
+// Cache first strategy for static and image files only
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+
+  // Check if the request URL is in the cached URLs set
+  if (cachedUrls.has(url.href)) {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return (
+          response ||
+          fetch(event.request, {
+            mode: "cors",
+            credentials: "omit",
+            redirect: "follow",
+          })
+        );
+      })
+    );
+  } else {
+    // For other requests, fetch from the network
+    event.respondWith(fetch(event.request));
+  }
+});
